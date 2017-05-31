578726a301c41bacc6f4deba798ed17e60e08e49
deedee96 <dan15011@byui.edu>
2017-05-31 13:30:55 -0600

    cleaned up comments

--- app/src/main/java/com/broulims/broulims/FrontPage.java
+++ app/src/main/java/com/broulims/broulims/FrontPage.java
@@ -4,14 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FrontPage extends AppCompatActivity {
-    // test comment for push -ben
-    //FIRST EVER PUSH :D
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page);
     }
 
-    //THIS WILL BE A MERGE CONFLICT
-    // best for last
+
 }
